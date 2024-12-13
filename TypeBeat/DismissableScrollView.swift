251421899136a92bded96a5c9e1a3e4dcc5d6637
Kirk Elliott <elliott@ionq.co>
2024-12-13 17:17:23 -0500

    updating images

---
+++ TypeBeat/DismissableScrollView.swift
@@ -0,0 +1,39 @@
+//
+//  DismissableScrollView.swift
+//  TypeBeat
+//
+//  Created by Kirk Elliott on 12/13/24.
+//
+
+
+import SwiftUI
+
+struct DismissableScrollView<Content: View>: UIViewRepresentable {
+    var content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    func makeUIView(context: Context) -> UIScrollView {
+        let scrollView = UIScrollView()
+        let hostingController = UIHostingController(rootView: content)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        hostingController.view.backgroundColor = .clear
+
+        scrollView.addSubview(hostingController.view)
+        NSLayoutConstraint.activate([
+            hostingController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            hostingController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        scrollView.keyboardDismissMode = .interactive // Enable swipe-to-dismiss
+        return scrollView
+    }
+
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        // No updates needed
+    }
+}
